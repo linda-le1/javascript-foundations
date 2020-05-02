932e09a3b544aab8d051ac474d30557c0bf3095e
Linda Le <yueling1222@gmail.com>
2020-05-02 13:46:39 -0600

    Add unicorn class

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,6 +1,21 @@
 class Unicorn {
-  constructor() {
+  constructor(name, color = 'white') {
+    this.name = name;
+    this.color = color;
+  }
+  isWhite() {
+    if (this.color != 'white') {
+      return false;
+    }
+  }
+  says(saying) {
+    var str_1 = "**;* "
+    var str_2 = saying
+    var str_3 = " *;**"
+    return str_1.concat(str_2, str_3);
   }
 }
+
+
 
 module.exports = Unicorn;
